f1ea83375c374c833ccbc1b28dbd726d50575db5
Vishnu0107 <vishnunarayananpsv@gmail.com>
2025-06-04 20:50:41 +0530

    Integrating nfs-nas

---
+++ include/repositories/nfs_repository.h
@@ -0,0 +1,32 @@
+#ifndef NFS_REPOSITORY_H_
+#define NFS_REPOSITORY_H_
+
+#include <nlohmann/json.hpp>
+
+#include "repository.h"
+
+class NFSRepository : public Repository {
+ public:
+  NFSRepository();
+  NFSRepository(const std::string& nfs_mount_path, const std::string& name,
+                const std::string& password, const std::string& created_at);
+  ~NFSRepository() {}
+
+  bool Exists() const override;
+  void Initialize() override;
+  void Delete() override;
+
+  void WriteConfig() const override;
+  static NFSRepository FromConfigJson(const nlohmann::json& config);
+
+ private:
+  std::string GetNFSPath() const;
+  bool UploadFile(const std::string& local_file,
+                  const std::string& remote_path) const;
+  bool NFSMountExists() const;
+  void EnsureNFSMounted() const;
+  void CreateRemoteDirectory() const;
+  void RemoveRemoteDirectory() const;
+};
+
+#endif  // NFS_REPOSITORY_H_
