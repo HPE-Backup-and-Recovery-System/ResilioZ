a79dad947681fe1594eae526a245f7920415c215
Nykaj A K <2nykajak@gmail.com>
2025-06-22 20:40:45 +0530

    Added basic UI and integration for scheduler module

--- include/gui/dialog/create_schedule_dialog.h
+++ include/gui/dialog/create_schedule_dialog.h
@@ -3,6 +3,10 @@
 
 #include <QDialog>
 
+#include "utils/scheduler_request_manager.h"
+#include "backup_restore/backup.hpp"
+#include "schedulers/schedule.h"
+
 namespace Ui {
 class CreateScheduleDialog;
 }
@@ -15,8 +19,28 @@ public:
     explicit CreateScheduleDialog(QWidget *parent = nullptr);
     ~CreateScheduleDialog();
 
+private slots:
+  void on_nextButton_clicked();
+  void on_backButton_clicked();
+
 private:
+    std::string cron_string;
+    std::string source_path;
+    std::string destination_path;
+    std::string remarks;
+    BackupType type;
+
     Ui::CreateScheduleDialog *ui;
+    SchedulerRequestManager *request_mgr;
+
+    void updateProgress();
+    void updateButtons();
+
+    bool handleEndpointDetails();
+    bool handleBackupDetails();
+    bool handleScheduleDetails();
+
+    void addSchedule();
 };
 
 #endif // CREATE_SCHEDULE_DIALOG_H
--- src/gui/dialog/create_schedule_dialog.cpp
+++ src/gui/dialog/create_schedule_dialog.cpp
@@ -1,14 +1,144 @@
 #include "gui/dialog/create_schedule_dialog.h"
 #include "gui/dialog/ui_create_schedule_dialog.h"
 
+#include "gui/decorators/message_box.h"
+#include "backup_restore/backup.hpp"
+#include "utils/utils.h"
+
 CreateScheduleDialog::CreateScheduleDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::CreateScheduleDialog)
 {
     ui->setupUi(this);
+    request_mgr = new SchedulerRequestManager();
+
+    cron_string="";
+    source_path="";
+    destination_path="";
+    remarks = "";
+    type = BackupType::FULL;
 }
 
 CreateScheduleDialog::~CreateScheduleDialog()
 {
     delete ui;
+    delete request_mgr;
+}
+
+void CreateScheduleDialog::updateProgress() {
+  int index = ui->stackedWidget_createSchedule->currentIndex();
+  int total = ui->stackedWidget_createSchedule->count();
+  int percent = ((index + 1) * 100) / total;
+  ui->progressBar->setValue(percent);
+}
+
+void CreateScheduleDialog::updateButtons() {
+  int index = ui->stackedWidget_createSchedule->currentIndex();
+  int total = ui->stackedWidget_createSchedule->count();
+  if (index == 0) {
+    // ui->backButton->setEnabled(false);
+  } else {
+    ui->backButton->setEnabled(true);
+  }
+  if (index == total - 1) {
+    ui->nextButton->setText("Create");
+  } else {
+    ui->nextButton->setText("Next");
+  }
+}
+
+void CreateScheduleDialog::on_nextButton_clicked() {
+  int index = ui->stackedWidget_createSchedule->currentIndex();
+  int total = ui->stackedWidget_createSchedule->count();
+
+  bool valid = true;
+  switch (index) {
+    case 0:
+      valid = handleEndpointDetails();
+      break;
+    case 1:
+      valid = handleBackupDetails();
+      break;
+    case 2:
+      valid = handleScheduleDetails();
+      break;
+    default:
+      break;
+  }
+  if (!valid) {
+    return;
+  }
+
+  if (index < total - 1) {
+    ui->stackedWidget_createSchedule->setCurrentIndex(index + 1);
+  } else {
+    ui->nextButton->setEnabled(false);
+    addSchedule();
+  }
+  updateProgress();
+}
+
+void CreateScheduleDialog::on_backButton_clicked() {
+  int index = ui->stackedWidget_createSchedule->currentIndex();
+  if (index > 0) {
+    ui->stackedWidget_createSchedule->setCurrentIndex(index - 1);
+  } else {
+    reject();
+  }
+  updateProgress();
+}
+
+bool CreateScheduleDialog::handleEndpointDetails(){
+    std::string source_path_ = ui->sourceInput->text().toStdString();
+    std::string destination_path_ = ui->destinationInput->text().toStdString();
+
+    if (!Validator::IsValidPath(source_path_)){
+        MessageBoxDecorator::showMessageBox(
+            this, "Invalid Source", "Source path is invalid.", QMessageBox::Warning);
+        return false;
+    }
+
+    if (!Validator::IsValidPath(destination_path_)){
+        MessageBoxDecorator::showMessageBox(
+            this, "Invalid Destination", "Destination path is invalid.", QMessageBox::Warning);
+        return false;
+    }
+
+    source_path = source_path_;
+    destination_path = destination_path_;
+    return true;
+}
+
+bool CreateScheduleDialog::handleBackupDetails(){
+    BackupType type_;
+    if (ui->incButton->isChecked()) {
+        type_ = BackupType::INCREMENTAL;
+    } else if (ui->diffButton->isChecked()) {
+        type_ = BackupType::DIFFERENTIAL;
+    } else {
+        type_ = BackupType::FULL;
+    }
+
+    std::string remarks_ = ui->remarksInput->text().toStdString();
+
+    type = type_;
+    remarks = remarks_;
+    return true;
+}
+
+bool CreateScheduleDialog::handleScheduleDetails(){
+    std::string cron_string_ = ui->scheduleInput->text().toStdString();
+
+    if (!Validator::IsValidCronString(cron_string_)){
+        MessageBoxDecorator::showMessageBox(
+            this, "Invalid cron string", "Cron string is invalid.", QMessageBox::Warning);
+        return false;
+    }
+    cron_string = cron_string_;
+    return true;
+}
+
+void CreateScheduleDialog::addSchedule(){
+    request_mgr-> SendAddRequest(cron_string, source_path, destination_path, remarks, type);
+    accept();
 }
